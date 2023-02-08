ff1135be65024dca8f057c47d79a3900a2b5bc79
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-02-08 18:47:57 -0300

    att

--- desafios/desafio013.py
+++ desafios/desafio013.py
@@ -3,4 +3,4 @@ s = float(input('Qual o salário do funcionário? R$'))
 a = s * 15 / 100
 novo = s + a
 
-print('O antigo salário era de R${:.2f}, o reajuste foi de R${:.2f}, o salário final é de R${:.2f}'.format(s, a, novo))
+print('O antigo salário era de R${:.2f}, o reajuste foi de 15% (R${:.2f}), o salário final é de R${:.2f}'.format(s, a, novo))
