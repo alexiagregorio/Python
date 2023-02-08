44b5a194baa9250460f2a9cb2a72bfa83262b61e
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-02-08 17:46:28 -0300

    att

--- desafios/desafio006.py
+++ desafios/desafio006.py
@@ -2,6 +2,6 @@ num = float(input('Digite um número: '))
 
 d = num * 2
 t = num * 3
-raiz = pow(num, 1/2) # calcula a raiz
+raiz = pow(num, 1/2) # calcula a raiz, também dava pra fazer => num ** (1/2)
 
-print('O dobro de {} é {}, \n o triplo é {}, \n e a raiz quadrada é {}.'.format(num, d, t, raiz))
+print('O dobro de {} é {}, \n o triplo é {}, \n e a raiz quadrada é {:.2f}.'.format(num, d, t, raiz))
