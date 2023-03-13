0042cbea334538b22becc2bdb97a1bfae138ec5e
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-03-13 15:21:46 -0300

    26

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -4,5 +4,7 @@
 #Em que posição ela aparece a primeira vez
 #Em que posição ela aparece a ultima vez
 ### 
-frase = input('Digite uma frase: ')
-print(frase.count('a'))
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
