161902dd20765a88baa03632f52b3f0efdc83e06
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-03-13 15:07:37 -0300

    desafio 24

--- desafios/desafio024.py
+++ desafios/desafio024.py
@@ -1,4 +1,4 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 
-cidade = input('Digite o nome de uma cidade: ')
-print(cidade.find('Santo'))
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+print(cidade[0:5].upper() == 'SANTO') #usa o upper pra jogar tudo em maiúscula e não ter problema se for escrito com maiúscula ou minúscula
