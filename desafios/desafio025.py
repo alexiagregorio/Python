230811ded3db970ad13d66a400b6fce973f5cb95
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-03-13 15:12:16 -0300

    desafio25

--- desafios/desafio025.py
+++ desafios/desafio025.py
@@ -1,4 +1,4 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 
-nome = input('Digite um nome: ')
-print(nome.find('Silva'))
+nome = str(input('Digite um nome: '))
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
